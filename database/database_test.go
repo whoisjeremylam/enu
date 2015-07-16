9f8079e402b54026da01b7db64596deca048f7c1
whoisjeremylam <jeremy@vennd.io>
2015-07-17 06:39:19 +0900

    Fixed test case

--- database/database_test.go
+++ database/database_test.go
@@ -1,10 +1,12 @@
 package database
 
 import "testing"
-import "github.com/vennd/enu/database"
 
 func TestGetNonceByAccesKey(t *testing.T) {
 	// Code to test nonce check
-	nonce := database.GetNonceByAccessKey("73a7b844c80c3c5cf532d1dd843321b1c733c0c67e5b5ab162ca283da4cfc182")
-	//	log.Printf("Nonce for %s is %d", "73a7b844c80c3c5cf532d1dd843321b1c733c0c67e5b5ab162ca283da4cfc182", nonce)
+	nonce := GetNonceByAccessKey("73a7b844c80c3c5cf532d1dd843321b1c733c0c67e5b5ab162ca283da4cfc182")
+
+	if nonce == 0 {
+		t.Errorf("Unable to retrieve nonce. Expected != 0, got: %d\n", nonce)
+	}
 }
