9c42228ca6ac78b5bc7ec44ffee8360b2780ef60
whoisjeremylam <jeremy@vennd.io>
2015-07-22 06:38:50 +0900

    All handlers should go through the Logger wrapper

--- router.go
+++ router.go
@@ -10,22 +10,22 @@ func NewRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", Logger(Index, "Index")).Methods("GET")
-	router.HandleFunc("/payment", PaymentCreate).Methods("POST")
-	router.HandleFunc("/payment/address", AddressCreate).Methods("POST")
-	router.HandleFunc("/payment/{paymentId}", GetPayment).Methods("GET")
-	router.HandleFunc("/payment/status/{paymentId}", PaymentRetry).Methods("POST")
+	router.HandleFunc("/payment", Logger(PaymentCreate, "PaymentCreate")).Methods("POST")
+	router.HandleFunc("/payment/address", Logger(AddressCreate, "AddressCreate")).Methods("POST")
+	router.HandleFunc("/payment/{paymentId}", Logger(GetPayment, "GetPayment")).Methods("GET")
+	router.HandleFunc("/payment/status/{paymentId}", Logger(PaymentRetry, "PaymentRetry")).Methods("POST")
 
 	router.HandleFunc("/asset", AssetCreate).Methods("POST")
-	router.HandleFunc("/asset/balances/{asset}", AssetBalance).Methods("GET")
-	router.HandleFunc("/asset/dividend", DividendCreate).Methods("POST")
-	router.HandleFunc("/asset/issuances/{asset}", AssetIssuances).Methods("GET")
-	router.HandleFunc("/asset/ledger/{asset}", AssetLedger).Methods("GET")
+	router.HandleFunc("/asset/balances/{asset}", Logger(AssetBalance, "AssetBalance")).Methods("GET")
+	router.HandleFunc("/asset/dividend", Logger(DividendCreate, "DividendCreate")).Methods("POST")
+	router.HandleFunc("/asset/issuances/{asset}", Logger(AssetIssuances, "AssetIssuances")).Methods("GET")
+	router.HandleFunc("/asset/ledger/{asset}", Logger(AssetLedger, "AssetLedger")).Methods("GET")
 
 	router.HandleFunc("/wallet", Logger(WalletCreate, "WalletCreate")).Methods("POST")
-	router.HandleFunc("/wallet/balances/{address}", WalletBalance).Methods("GET")
+	router.HandleFunc("/wallet/balances/{address}", Logger(WalletBalance, "WalletBalance")).Methods("GET")
 	router.HandleFunc("/wallet/payment", Logger(WalletSend, "WalletSend")).Methods("POST")
 
-	router.HandleFunc("/blocks", GetBlocks).Methods("GET")
+	router.HandleFunc("/blocks", Logger(GetBlocks, "GetBlocks")).Methods("GET")
 
 	return router
 }
