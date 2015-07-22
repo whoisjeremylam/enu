9695338c52e6df25de1953049a9cd29dac89cc22
Jeremy Lam <jeremy@vennd.io>
2015-07-22 10:07:32 +0900

    Trigger codeship

--- enu.go
+++ enu.go
@@ -8,6 +8,6 @@ import (
 func main() {
 	router := NewRouter()
 
-	log.Println("Enu API server started!")
+	log.Println("Enu API server started")
 	log.Fatal(http.ListenAndServe("localhost:8080", router))
 }
