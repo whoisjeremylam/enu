6df24f3244ab5b671c69d4669da2a2542f31ad04
whoisjeremylam <jeremy@vennd.io>
2015-07-27 11:33:03 +0900

    Added comments

--- logger.go
+++ logger.go
@@ -9,8 +9,8 @@ import (
 func Logger(fn http.HandlerFunc, name string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		fn(w, r)
-		log.Printf(
+		fn(w, r)			// Call the real handler
+		log.Printf(			// Log how long it took
 			"%s,%s,%s,%s",
 			r.Method,
 			r.RequestURI,
