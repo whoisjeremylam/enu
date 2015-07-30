e4aaefcf198293cc7ca1fe94bffee7963396c9ec
whoisjeremylam <jeremy@vennd.io>
2015-07-30 11:42:54 +0900

    Modified test case and added a test case for get blocks

--- bitcoinapi/bitcoinapi_test.go
+++ bitcoinapi/bitcoinapi_test.go
@@ -9,7 +9,6 @@ import (
 var destinationAddress string = "13Yoi8aMj5ygY8dKx17vxsTMkHNP9SVayy"
 
 func TestGetBalance(t *testing.T) {
-	
 	result, err := GetBalance(destinationAddress)
 
 	if err != nil {
@@ -19,8 +18,24 @@ func TestGetBalance(t *testing.T) {
 	// 
 	
 	if result < 0  { 
-		t.Errorf("Balance is too small\n")
+		t.Errorf("Expected: balance >= 0, received: %f\n", result)
 		
 	}
 
+}
+
+
+func TestGetGetBlockCount(t *testing.T) {
+	result, err := GetBlockCount()
+
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	// 
+	
+	if result < 367576  { 
+		t.Errorf("Expected: block height > 367576, received: %d\n", result)
+		
+	}
 }
