f24b6f11fdf43373596456894f6134c2b32be23d
bbaas1 <sebastian@vennd.io>
2015-07-30 14:50:36 +1000

    sb test

--- bitcoinapi/bitcoinapi_test.go
+++ bitcoinapi/bitcoinapi_test.go
@@ -9,6 +9,7 @@ import (
 var destinationAddress string = "13Yoi8aMj5ygY8dKx17vxsTMkHNP9SVayy"
 
 func TestGetBalance(t *testing.T) {
+	
 	result, err := GetBalance(destinationAddress)
 
 	if err != nil {
@@ -16,26 +17,10 @@ func TestGetBalance(t *testing.T) {
 	}
 
 	// 
-	
+	println(result)
 	if result < 0  { 
-		t.Errorf("Expected: balance >= 0, received: %f\n", result)
+		t.Errorf("Balance is too small\n", result)
 		
 	}
 
-}
-
-
-func TestGetGetBlockCount(t *testing.T) {
-	result, err := GetBlockCount()
-
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-
-	// 
-	
-	if result < 367576  { 
-		t.Errorf("Expected: block height > 367576, received: %d\n", result)
-		
-	}
 }
