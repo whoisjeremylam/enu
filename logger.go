2628e847ffa31af64580165e23fe7e05ce85358d
whoisjeremylam <jeremy@vennd.io>
2015-09-04 22:11:30 +0900

    Move to logging via fluent

--- logger.go
+++ logger.go
@@ -3,7 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
-	"log"
+	//	"log"
 	"net/http"
 	"sort"
 	"strings"
@@ -12,6 +12,7 @@ import (
 	"github.com/vennd/enu/consts"
 	"github.com/vennd/enu/database"
 	"github.com/vennd/enu/enulib"
+	"github.com/vennd/enu/log"
 
 	"github.com/vennd/enu/internal/golang.org/x/net/context"
 )
@@ -22,21 +23,6 @@ type appError struct {
 	Code    int
 }
 
-/*func Logger(fn http.HandlerFunc, name string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		fn(w, r)    // Call the real handler
-		log.Printf( // Log how long it took
-			"%s,%s,%s,%s",
-			r.Method,
-			r.RequestURI,
-			name,
-			time.Since(start),
-		)
-	}
-}
-*/
-
 type ctxHandler func(context.Context, http.ResponseWriter, *http.Request) *appError
 
 func (fn ctxHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -44,18 +30,20 @@ func (fn ctxHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	// Generate an requestId
 	requestId := enulib.GenerateRequestId()
-	log.Printf("Generated requestId: %s", requestId)
 
 	// Create a context with only the requestId in it initially
 	parent := context.TODO()
 	ctx := context.WithValue(parent, consts.RequestIdKey, requestId)
+
+	log.FluentfContext(sourceFile, ctx, "%s %s entered.", r.Method, r.URL.Path)
 
 	accessKey, nonceInt, err := CheckHeaderGeneric(ctx, w, r)
 	if err != nil {
 		return
 	}
 
-	log.Printf("accessKey for request: %s", accessKey)
+	// Add to the context the accessKey now that we know it
+	ctx1 := context.WithValue(ctx, consts.AccessKeyKey, accessKey)
 
 	database.UpdateNonce(accessKey, nonceInt)
 	if err != nil {
@@ -81,12 +69,14 @@ func (fn ctxHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	// If the blockchain specified in the path isn't valid and a default blockchainId isn't set in the userkey then fail
 	if blockchainValid == false && userBlockchainIdValid == false {
+		log.FluentfContext(sourceFile, ctx, "Unsupported blockchain. Valid values: %s", strings.Join(supportedBlockchains, ", "))
 		e := fmt.Sprintf("Unsupported blockchain. Valid values: %s", strings.Join(supportedBlockchains, ", "))
 		ReturnServerError(ctx, w, errors.New(e))
 
 		return
 	}
 
+	// If the blockchain was explicitly given in the resource path, use it. Otherwise use the blockchain from the user's profile
 	var blockchainId string
 	if blockchainValid == true {
 		blockchainId = requestBlockchainId
@@ -94,20 +84,19 @@ func (fn ctxHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		blockchainId = usersDefaultBlockchain
 	}
 
-	log.Printf("Operating on blockchain: %s\n", blockchainId)
-
-	// Add accessKey and blockchainId to the context - nonce has been dropped from context since it has already been updated in the DB
-	ctx1 := context.WithValue(ctx, consts.AccessKeyKey, accessKey)
-	//	ctx = context.WithValue(ctx1, consts.NonceIntKey, nonceInt)
+	// Add blockchain to the context now that we know it
 	ctx = context.WithValue(ctx1, consts.BlockchainIdKey, blockchainId)
+
+	log.FluentfContext(sourceFile, ctx, "Calling context function.")
 
 	// run function
 	if e := fn(ctx, w, r); e != nil { // e is *appError, not os.Error.
 		http.Error(w, e.Message, e.Code)
 	}
 
-	log.Printf( // Log how long it took
-		"%s,%s,%s,%s",
+	// Log how long it took
+	log.FluentfContext(sourceFile, ctx,
+		"%s,%s,%s",
 		r.Method,
 		r.RequestURI,
 		time.Since(start),
