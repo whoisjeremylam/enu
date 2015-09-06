88a8247747c6535113fc3b1f818c7c7b4a62bba3
whoisjeremylam <jeremy@vennd.io>
2015-09-06 23:16:41 +0900

    Display env and hostname on start up

--- enu.go
+++ enu.go
@@ -3,11 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	env := os.Getenv("ENV")
+	hostname, err := os.Hostname()
+
+	if err != nil {
+		hostname = "unknown environment"
+	}
+
+	if env == "" {
+		env = "unknown host"
+	}
+
 	router := NewRouter()
 
-	log.Println("Enu API server started")
+	log.Printf("Enu %s API server started on %s", env, hostname)
 	log.Fatal(http.ListenAndServe("localhost:8080", router))
 }
