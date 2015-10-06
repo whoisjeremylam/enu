64c0021d9b8f60b1a4ef5bac0b8ebad300704a75
whoisjeremylam <jeremy@vennd.io>
2015-10-07 06:18:13 +0900

    Creates a csv dump of all errors defined in consts/errors

---
+++ utils/dumperrors/dumperrors.go
@@ -0,0 +1,40 @@
+//Dumps all errors into a CSV for easy digestion
+package main
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+
+	"github.com/vennd/enu/consts"
+	"github.com/vennd/enu/log"
+)
+
+func printHeader(f *os.File) {
+	// Print out header
+	fmt.Println("\"Error Group\",\"Error Code\", \"Error Description\"")
+	f.Write([]byte("\"Error Group\",\"Error Code\", \"Error Description\""))
+}
+
+func printGroup(f *os.File, groupName string, group interface{}) {
+	v := reflect.ValueOf(group)
+	for i := 0; i < v.NumField(); i++ {
+		fmt.Printf("\"%s\", %d, \"%s\"\n", groupName, v.Field(i).Interface().(consts.ErrCodes).Code, v.Field(i).Interface().(consts.ErrCodes).Description)
+	}
+}
+
+func main() {
+	// Open file
+	f, err := os.Create("errors.txt")
+	if err != nil {
+		log.Fluentf(consts.LOGERROR, err.Error())
+		return
+	}
+	defer f.Close()
+
+	printHeader(f)
+
+	// Range through each error group
+	printGroup(f, "Generic Errors", consts.GenericErrors)
+	printGroup(f, "Counterparty Errors", consts.CounterpartyErrors)
+}
