3356542862978851c668f2b2761f2596dda1afcd
whoisjeremylam <jeremy@vennd.io>
2015-10-16 10:32:44 +0900

    Change cycle times for monitoring

--- monitoring/pingbitcoind/restartAndAlertBitcoin.sh
+++ monitoring/pingbitcoind/restartAndAlertBitcoin.sh
@@ -6,5 +6,5 @@ sudo sv restart bitcoin
 curl -s \
   --form-string "token=ah1GHznmo7xUxqVrcqP59TeD1h6CS8" \
   --form-string "user=gTgS3iypxztM2SgvLYf3JwrSkactFb" \
-  --form-string "message=$MONIT_HOST: Bitcoind has fallen behind in processing blocks. bitcoind has been restarted" \
+  --form-string "message=$MONIT_HOST: Bitcoind has failed or fallen behind in processing blocks. bitcoind has been restarted" \
   https://api.pushover.net/1/messages.json
--- monitoring/pingcounterpartyd/restartAndAlertCounterparty.sh
+++ monitoring/pingcounterpartyd/restartAndAlertCounterparty.sh
@@ -7,5 +7,5 @@ sudo sv restart counterparty
 curl -s \
   --form-string "token=ah1GHznmo7xUxqVrcqP59TeD1h6CS8" \
   --form-string "user=gTgS3iypxztM2SgvLYf3JwrSkactFb" \
-  --form-string "message=$MONIT_HOST: Counterpartyd service became unresponsive and has been restarted" \
+  --form-string "message=$MONIT_HOST: Counterpartyd service was down and has been restarted" \
   https://api.pushover.net/1/messages.json
