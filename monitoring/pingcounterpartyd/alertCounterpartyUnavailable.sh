be7fd3b565e0e043e8499cc3acd9b87ac8049965
whoisjeremylam <jeremy@vennd.io>
2015-10-21 10:57:36 +0900

    Change message on push notification when counterpartyd unavailable

--- monitoring/pingcounterpartyd/alertCounterpartyUnavailable.sh
+++ monitoring/pingcounterpartyd/alertCounterpartyUnavailable.sh
@@ -4,5 +4,5 @@
 curl -s \
   --form-string "token=ah1GHznmo7xUxqVrcqP59TeD1h6CS8" \
   --form-string "user=gTgS3iypxztM2SgvLYf3JwrSkactFb" \
-  --form-string "message=$MONIT_HOST: Counterpartyd service has been unavailable for 30 minutes. Please check the service. Monitoring has been paused." \
+  --form-string "message=$MONIT_HOST: Counterpartyd service has been temporarily unavailable for more than 30 minutes. Please check the service." \
   https://api.pushover.net/1/messages.json
