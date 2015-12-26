1d1a42c675254795ee313a489f087cd92f656eb8
whoisjeremylam <jeremy@vennd.io>
2015-12-27 05:02:43 +0900

    Add Ripple parameter validation. Closes https://github.com/vennd/enu/issues/96

--- consts/validation.go
+++ consts/validation.go
@@ -2,16 +2,20 @@ package consts
 
 type Validations map[string]string
 
+// cf http://spacetelescope.github.io/understanding-json-schema/
 var ParameterValidations = map[string]Validations{
 	"counterparty": {
-		"asset":           `{"properties":{"sourceAddress":{"type":"string", "maxLength":34, "minLength":34},"description":{"type":"string"},"asset":{"type":"string","minLength":4},"quantity":{"type":"integer"},"divisible":{"type":"boolean"}},"required":["sourceAddress","asset","quantity","divisible"]}`,
-		"dividend":        `{"properties":{"sourceAddress":{"type":"string", "maxLength":34, "minLength":34},"asset":{"type":"string","minLength":4},"dividendAsset":{"type":"string"},"quantityPerUnit":{"type":"integer"}},"required":["sourceAddress","asset","dividendAsset","quantityPerUnit"]}`,
-		"walletCreate":    `{"properties":{"numberOfAddresses":{"type":"integer"}}}`,
-		"walletPayment":   `{"properties":{"sourceAddress":{"type":"string", "maxLength":34, "minLength":34},"destinationAddress":{"type":"string", "maxLength":34, "minLength":34},"asset":{"type":"string","minLength":4},"quantity":{"type":"integer"}},"required":["sourceAddress","asset","quantity","destinationAddress"]}`,
-		"simplePayment":   `{"properties":{"sourceAddress":{"type":"string", "maxLength":34, "minLength":34},"destinationAddress":{"type":"string", "maxLength":34, "minLength":34},"asset":{"type":"string","minLength":4},"amount":{"type":"integer"},,"txFee":{"type":"integer"}},"required":["sourceAddress","destinationAddress","asset","amount"]}`,
-		"activateaddress": `{"properties":{"address":{"type":"string","maxLength":34,"minLength":34},"amount":{"type":"integer"}},"required":["address","amount"]}`,
+		"asset":           `{"properties":{"blockchainId":{"type":"string"},"passphrase":{"type":"string"},"distributionAddress":{"type":"string","maxLength":34,"minLength":34},"distributionPassphrase":{"type":"string"},"description":{"type":"string"},"asset":{"type":"string","minLength":4},"quantity":{"type":"integer"},"divisible":{"type":"boolean"},"nonce":{"type":"integer"}},"required":["sourceAddress","passphrase","asset","quantity","divisible"]}`,
+		"dividend":        `{"properties":{"blockchainId":{"type":"string"},"sourceAddress":{"type":"string","maxLength":34,"minLength":34},"asset":{"type":"string","minLength":4},"dividendAsset":{"type":"string"},"quantityPerUnit":{"type":"integer"},"nonce":{"type":"integer"}},"required":["sourceAddress","asset","dividendAsset","quantityPerUnit"]}`,
+		"walletCreate":    `{"properties":{"blockchainId":{"type":"string"},"numberOfAddresses":{"type":"number","minimum":1,"maximum":100,"exclusiveMaximum":false},"nonce":{"type":"integer"}}}`,
+		"walletPayment":   `{"properties":{"blockchainId":{"type":"string"},"sourceAddress":{"type":"string","maxLength":34,"minLength":34},"destinationAddress":{"type":"string","maxLength":34,"minLength":34},"asset":{"type":"string","minLength":4},"quantity":{"type":"integer"},"nonce":{"type":"integer"}},"required":["sourceAddress","asset","quantity","destinationAddress"]}`,
+		"simplePayment":   `{"properties":{"sourceAddress":{"type":"string", "maxLength":34, "minLength":34},"destinationAddress":{"type":"string", "maxLength":34, "minLength":34},"asset":{"type":"string","minLength":4},"amount":{"type":"integer"},"txFee":{"type":"integer"}},"required":["sourceAddress","destinationAddress","asset","amount"]}`,
+		"activateaddress": `{"properties":{"blockchainId":{"type":"string"},"address":{"type":"string","maxLength":34,"minLength":34},"amount":{"type":"integer"},"nonce":{"type":"integer"}},"required":["address","amount"]}`,
 	},
 	"ripple": {
-		"walletPayment": `{"properties":{"sourceAddress":{"type":"string", "maxLength":34, "minLength":34},"destinationAddress":{"type":"string", "maxLength":34, "minLength":34},"asset":{"type":"string","minLength":3},"quantity":{"type":"integer"}},"required":["sourceAddress","asset","quantity","destinationAddress"]}`,
+		"asset":           `{"properties":{"blockchainId":{"type":"string"},"passphrase":{"type":"string"},"distributionAddress":{"type":"string","maxLength":34,"minLength":34},"distributionPassphrase":{"type":"string"},"description":{"type":"string"},"asset":{"type":"string","minLength":4},"quantity":{"type":"integer"},"divisible":{"type":"boolean"},"nonce":{"type":"integer"}},"required":["sourceAddress","passphrase","asset","quantity","divisible"]}`,
+		"walletCreate":    `{"properties":{"blockchainId":{"type":"string"},"nonce":{"type":"integer"}}}`,
+		"walletPayment":   `{"properties":{"blockchainId":{"type":"string"},"sourceAddress":{"type":"string","maxLength":34,"minLength":34},"destinationAddress":{"type":"string","maxLength":34,"minLength":34},"asset":{"type":"string","minLength":3},"quantity":{"type":"integer"},"nonce":{"type":"integer"}},"required":["sourceAddress","asset","quantity","destinationAddress"]}`,
+		"activateaddress": `{"properties":{"blockchainId":{"type":"string"},"address":{"type":"string","maxLength":34,"minLength":34},"passphrase":{"type":"string"},"amount":{"type":"integer"},"assets":{"type":"object","properties":{"currency":{"type":"string"},"issuer":{"type":"string"}}},"nonce":{"type":"integer"}},"required":["address","amount"]}`,
 	},
 }
