0dfdcfdc28361f91454d54bc0839ec9cb8e587a6
whoisjeremylam <jeremy@vennd.io>
2016-01-29 05:50:02 +0900

    Remove quotes

--- utils/dumperrors/dumperrors.go
+++ utils/dumperrors/dumperrors.go
@@ -12,14 +12,14 @@ import (
 
 func printHeader(f *os.File) {
 	// Print out header
-	fmt.Println("\"Error Group\",\"Error Code\", \"Error Description\"")
-	f.Write([]byte("\"Error Group\",\"Error Code\", \"Error Description\""))
+	fmt.Println("Error Group,Error Code, Error Description")
+	f.Write([]byte("Error Group,Error Code, Error Description"))
 }
 
 func printGroup(f *os.File, groupName string, group interface{}) {
 	v := reflect.ValueOf(group)
 	for i := 0; i < v.NumField(); i++ {
-		t := fmt.Sprintf("\"%s\", %d, \"%s\"\n", groupName, v.Field(i).Interface().(consts.ErrCodes).Code, v.Field(i).Interface().(consts.ErrCodes).Description)
+		t := fmt.Sprintf("%s, %d, %s\n", groupName, v.Field(i).Interface().(consts.ErrCodes).Code, v.Field(i).Interface().(consts.ErrCodes).Description)
 		if v.Field(i).Interface().(consts.ErrCodes).Code != 0 {
 			fmt.Printf(t)
 			f.Write([]byte(t))
