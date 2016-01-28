2e576aa335bd7eed1cd8f7f0e214300957a5ced6
whoisjeremylam <jeremy@vennd.io>
2016-01-29 05:42:56 +0900

    Remove commas from error messages. Helps with conversion from csv to markdown

--- consts/errors.go
+++ consts/errors.go
@@ -69,7 +69,7 @@ var GenericErrors = GenericStruct{
 	InvalidDocument:       ErrCodes{1, "There was a problem with the parameters in your JSON request. Please correct the request."},
 	InvalidDividendId:     ErrCodes{2, "The specified dividend id is invalid."},
 	UnsupportedBlockchain: ErrCodes{3, "The specified blockchain is not supported."},
-	HeadersIncorrect:      ErrCodes{4, "Request headers were not set correctly, ensure the following headers are set: accessKey and signature."},
+	HeadersIncorrect:      ErrCodes{4, "Request headers were not set correctly ensure the following headers are set: accessKey and signature."},
 	UnknownAccessKey:      ErrCodes{5, "Attempt to access API with unknown user key"},
 	InvalidSignature:      ErrCodes{6, "Could not verify HMAC signature"},
 	InvalidNonce:          ErrCodes{7, "Invalid nonce"},
@@ -81,7 +81,7 @@ var GenericErrors = GenericStruct{
 	GeneralError:          ErrCodes{13, "Misc error. Please contact Vennd.io support."},
 	InvalidAddress:        ErrCodes{14, "The specified address is invalid. Please correct the address and resubmit."},
 	InvalidAsset:          ErrCodes{15, "The specified asset is invalid. Please correct the asset and resubmit."},
-	ApiKeyDisabled:        ErrCodes{16, "The specified API is valid. However, it has been disabled by an administrator."},
+	ApiKeyDisabled:        ErrCodes{16, "The specified API is valid. However it has been disabled by an administrator."},
 }
 
 type RippleStruct struct {
@@ -107,13 +107,13 @@ var RippleErrors = RippleStruct{
 	InvalidAmount:                 ErrCodes{2002, "The amount specified is not a valid amount."},
 	InvalidCurrency:               ErrCodes{2003, "The currency is invalid. Ripple currencies must be 3 characters or longer."},
 	SubmitError:                   ErrCodes{2004, "The Ripple node rejected the transaction submission. Please try again."},
-	IssuerMustBeGiven:             ErrCodes{2005, "If the currency is not XRP, the issuer must be provided."},
+	IssuerMustBeGiven:             ErrCodes{2005, "If the currency is not XRP the issuer must be provided."},
 	SigningError:                  ErrCodes{2006, "Unable to sign transaction. Is your passphrase correct?"},
 	SubmitErrorFeeLost:            ErrCodes{2007, "The transaction was submitted to the Ripple network but was invalid."},
-	InvalidCurrencyOrNoTrustline:  ErrCodes{2008, "The specified asset is invalid, or you must activate the destination wallet to accept the asset."},
+	InvalidCurrencyOrNoTrustline:  ErrCodes{2008, "The specified asset is invalid or you must activate the destination wallet to accept the asset."},
 	InvalidSource:                 ErrCodes{2009, "The specified source address is invalid."},
 	InvalidDestination:            ErrCodes{2010, "The specified destination address is invalid."},
-	DistributionPassphraseMissing: ErrCodes{2011, "If a distribution address is specified, the passphrase for the distribution address must be given."},
+	DistributionPassphraseMissing: ErrCodes{2011, "If a distribution address is specified the passphrase for the distribution address must be given."},
 	DistributionInsufficientFunds: ErrCodes{2012, "The specified distribution address does not contain sufficient funds. Please activate the address and try again."},
 	InsufficientXRP:               ErrCodes{2013, "There was insufficient XRP in the address to perform the payment. Please activate the address and try again."},
 }
